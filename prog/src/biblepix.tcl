95597954c6537aa5e170b4be32784896aebbd8f8
linuxpeter <linux@vollmar.ch>
2017-05-07 17:42:41 +0200

    Pfad sharedir geändert nach 'com'

--- prog/src/biblepix.tcl
+++ prog/src/biblepix.tcl
@@ -3,12 +3,12 @@
 # Projects The Word from "Bible 2.0" on a daily changing backdrop image 
 # OR displays The Word in the terminal OR adds The Word to e-mail signatures
 # Authors: Peter Vollmar, Joel Hochreutener, biblepix.vollmar.ch
-# Updated: 20feb17
+# Updated: 7mai17
 ######################################################################
 
 #Verify location & source Globals
 set srcdir [file dirname [info script]]
-set Globals "[file join $srcdir share globals.tcl]"
+set Globals "[file join $srcdir com globals.tcl]"
 source $Globals
 
 #Parse TWD file & get text for all functions - or start Setup
