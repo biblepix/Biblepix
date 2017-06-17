80a2aa597cc49e83787e45b8194facc8176a4c77
linuxpeter <linux@vollmar.ch>
2017-06-17 12:32:44 +0200

    proc checkImgSize nach imgtools verschoben proc rebuildBMPs löschen?

--- prog/src/pic/hgbild.tcl
+++ prog/src/pic/hgbild.tcl
@@ -1,7 +1,7 @@
 # ~/Biblepix/progs/src/pic/hgbild.tcl
 # Creates background picture, called by image.tcl
 # Authors: Peter Vollmar & Joel Hochreutener, biblepix.vollmar.ch
-# Updated 16mai2017
+# Updated 15jun2017
 
 source $Imgtools  
 
@@ -74,6 +74,9 @@ global  fontcolor fgrgb sun shade
     	
 } ;#end fgbild>hgbild
 
+
+
+#TODO - move to ???? - NOT CALLED BY ANY PROG !!!!!!!!!!!!!!!!!!!!!!!!!!!
 proc checkRebuildBMP {} {
 #Recreate fgbild if end of row pixels are bg-colour
 #instead of font colour (=unfinished pic)
@@ -97,115 +100,5 @@ set errhex [format #%02x%02x%02x $r $g $b]
         }
 } ;#end checkRebuildBMP
 
-proc checkImgSize {} {
-#called by image.tcl
-#rezises background JPEGs
-	global hgfile screenx screeny jpegdir Imgtools
-	
-	set imgx [image width hgbild]
-	set imgy [image height hgbild]
-	#diffs -/+
-	set diffx [expr $screenx-$imgx]
-	set diffy [expr $screeny-$imgy]
-	
-	variable factor 0
-	
-	## 1. P R E  Z O O M
-	#Double or halve img if different from screen
-	
-	#beg MAIN
-	if {$diffx || $diffy} {
-		# Bild zu breit
-		if {$diffx<0} {
-			#abrunden, damit nicht zu klein
-			set factor [expr $imgx/$screenx]
-			set zoom -subsample
-		# Bild zu schmal
-		} elseif {$diffx>200} {
-			#aufrunden, damit nicht zu klein
-			set zoom -zoom
-			set factor [expr round($screenx./$imgx)]
-		}
-		
-		#Zoom either way
-		if {$factor>1} {
-			#??	if {$zoom=="-zoom"} {set zoomvar up} else {set zoomvar down}
-			image create photo tmpimg
-			tmpimg copy hgbild $zoom $factor
-			hgbild blank
-			hgbild copy tmpimg -shrink 
-		} 		
-		
-		#reset vars
-		set imgx [image width hgbild]
-		set imgy [image height hgbild]
-		#diffs - oder +
-		set diffx [expr $screenx-$imgx]
-		set diffy [expr $screeny-$imgy]
-		
-	## 2. F I N E - C U T  - correct uneven sides
-	#Syntax: x1 y1 x2 y2
-	
-	#check if sides uneven
-		if {$diffx==$diffy} {} else {
-			source $Imgtools
-			puts "Correcting uneven image edges..."
-		#1. diffx und diffy - (grösser als screen)
-			if {$diffx<0 && $diffy<0} {
-				regsub {\-} $diffx {} diffx
-				regsub {\-} $diffy {} diffy
-				if {$diffx>$diffy } {
-					set diff [expr $diffx-$diffy]
-					cutx hgbild $diff
-				} else {
-					set diff [expr $diffy-$diffx]
-					cuty hgbild $diff
-				}
-		#2. diffx & diffy + (kleiner als screen)
-			} elseif {$diffx>0 && $diffy>0} {
-				if {$diffx>$diffy } {
-					set diff [expr $diffx-$diffy]
-					cutx hgbild $diff
-				} else {
-					set diff [expr $diffy-$diffx]
-					cuty hgbild $diff
-				}
-	    #3. diffs einmal + und einmal - 
-			} else {
-				#X im Minus
-				if {$diffx < $diffy} {
-					regsub {\-} $diffx {} diffx
-					set diff [expr $diffy+$diffx]
-					cutx hgbild $diff
-				#Y im Minus
-				} else {
-					regsub {\-} $diffy {} diffy
-					set diff [expr $diffy+$diffx]
-					cuty hgbild $diff
-				} 
-			}
-		} ;#end fine cut
-		
-		#set new dimensions
-		set imgx [image width hgbild]
-		set imgy [image height hgbild]
-		puts "Image after cutting: $imgx $imgy  "
-		
-	## 3. R E S I Z E  even-sided hgbild if necessary
-	
-		if {$screenx!=$imgx} {
-			source $Imgtools
-			image create photo imgneu -width 0 -height 0
-			resize hgbild $screenx $screeny imgneu
-			hgbild blank
-			hgbild copy imgneu -shrink
-		}
-		
-	
-	#Save corrected image
-		hgbild write $hgfile -format JPEG
-		puts "Saving resized image [image width hgbild]x[image height hgbild] to $jpegdir"
-		
-	} ;#end MAIN
 
-} ;#end checkImageSize
+
