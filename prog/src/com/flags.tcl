c6215e406f0c18bdedb77f0b6a566d8a0de4de88
Joel Hochreutener <cjhox9@gmail.com>
2017-09-22 15:39:49 +0200

    flag datei korrigiert

--- prog/src/com/flags.tcl
+++ prog/src/com/flags.tcl
@@ -34,8 +34,7 @@
         set r [expr $h*0.3]
         set xm [expr ($x+$w)/2]
         set ym [expr ($y+$h)/2+2]
-        $c create oval [expr $xm-$r] [expr $ym-$r]\
-            [expr $xm+$r] [expr $ym+$r] -fill $color -outline $color
+        $c create oval [expr $xm-$r] [expr $ym-$r] [expr $xm+$r] [expr $ym+$r] -fill $color -outline $color
     }
     proc hori args {
         upvar b b c c x x y y w w h h
@@ -44,8 +43,7 @@
         set dy [expr $h/$n.]
         set y0 [expr $y+2+$b]
         foreach i $args {
-            $c create rect $x $y0 [expr $x+$w+2*$b]\
-                [expr round($y0+$dy)] -fill $i -outline $i
+            $c create rect $x $y0 [expr $x+$w+2*$b] [expr round($y0+$dy)] -fill $i -outline $i
             set y0 [expr $y0+$dy]
         }
     }
@@ -56,8 +54,7 @@
         set dx [expr $w/$n.]
         set x0 [expr $x+2]
         foreach i $args {
-            $c create rect $x0 $y [expr round($x0+$dx)] [expr $y+$w]\
-                -fill $i -outline $i
+            $c create rect $x0 $y [expr round($x0+$dx)] [expr $y+$w] -fill $i -outline $i
             set x0 [expr $x0+$dx]
         }
     }
@@ -68,10 +65,8 @@
         set y1 [expr $y+$h*0.4+2]
         set y2 [expr $h*0.6+1]
         foreach i $args {
-            $c create rect $x $y1 [expr $x+$w+2*$b] $y2\
-                -fill $i -outline $i
-            $c create rect $x1 $y $x2 [expr $y+$h+2]\
-                -fill $i -outline $i
+            $c create rect $x $y1 [expr $x+$w+2*$b] $y2 -fill $i -outline $i
+            $c create rect $x1 $y $x2 [expr $y+$h+2] -fill $i -outline $i
             set x1 [expr $x1*1.1]
             set x2 [expr $x2/1.1]
             set y1 [expr $y1*1.1]
@@ -83,10 +78,8 @@
         set y1 [expr $h*0.4+2]
         set y2 [expr $h*0.6]
         foreach i $args {
-            $c create rect $x $y1 [expr $x+$w+2*$b] $y2\
-                -fill $i -outline $i
-            $c create rect $y1 $y $y2 [expr $y+$h+1]\
-                -fill $i -outline $i
+            $c create rect $x $y1 [expr $x+$w+2*$b] $y2 -fill $i -outline $i
+            $c create rect $y1 $y $y2 [expr $y+$h+1] -fill $i -outline $i
             set y1 [expr $y1*1.1]
             set y2 [expr $y2/1.1]
         }
@@ -101,19 +94,15 @@
         set y1 [expr $y+$h*0.4+2+$b]
         set y2 [expr $y+$h*0.6]
         set y3 [expr $y+$h*0.8]
-        $c create rect $x0 $y1 $x3 $y2\
-                -fill $color -outline $color
-        $c create rect $x1 $y0 $x2 $y3\
-                -fill $color -outline $color
+        $c create rect $x0 $y1 $x3 $y2 -fill $color -outline $color
+        $c create rect $x1 $y0 $x2 $y3 -fill $color -outline $color
     }
     proc x args {
         upvar c c x x y y w w h h
         set width [expr round($w/10.)]
         foreach i $args {
-            $c create line $x [expr $y+2] [expr $x+$w] [expr $y+$h]\
-                -fill $i -width $width
-            $c create line $x [expr $y+$h] [expr $x+$w] [expr $y+2] \
-                -fill $i -width $width
+            $c create line $x [expr $y+2] [expr $x+$w] [expr $y+$h] -fill $i -width $width
+            $c create line $x [expr $y+$h] [expr $x+$w] [expr $y+2] -fill $i -width $width
             set width [expr int($width/2.)]
         }
     }
@@ -124,9 +113,7 @@
         set y0 [expr $y+2+$b]
         for {set i 0} {$i<$n} {incr i} {
             set color [set c[expr $i%2]]
-            $c create rect $x $y0 [expr $x+$w+2*$b] \
-                [expr round($y0+$dy)]\
-                -fill $color -outline $color
+            $c create rect $x $y0 [expr $x+$w+2*$b] [expr round($y0+$dy)] -fill $color -outline $color
             set y0 [expr $y0+$dy]
         }
     }
@@ -135,9 +122,7 @@
         set x0 [expr $x+$w/2.+2]
         set y0 [expr $y+$h/2.+2]
         set r [expr round($h/2.25)]
-        $c create oval [expr $x0-$r/2.] [expr $y0-$r/2.]\
-            [expr $x0+$r/2.] [expr $y0+$r/2.]\
-            -fill $color
+        $c create oval [expr $x0-$r/2.] [expr $y0-$r/2.] [expr $x0+$r/2.] [expr $y0+$r/2.] -fill $color
         foreach i [geom::sunrays $x0 $y0 $r] {
             eval $c create poly $i -fill $color
         }
@@ -161,8 +146,7 @@
     proc triangle color {
         upvar c c x x y y w w h h
         set x1 [expr sqrt(3*($h/2.)*($h/2.))]
-        $c create poly $x $y $x1 [expr ($y+$h)/2.+2] $x [expr $y+$h+2]\
-            -fill $color
+        $c create poly $x $y $x1 [expr ($y+$h)/2.+2] $x [expr $y+$h+2] -fill $color
     }
     proc left amount {upvar w w; set w [expr $w*$amount]}
     proc moon color {
@@ -172,12 +156,10 @@
         set item [$c find closest $x0 $y0]
         set bg [$c itemcget $item -fill]
         set r [expr round($h/4.)]
-        $c create oval [expr $x0-$r] [expr $y0-$r] \
-            [expr $x0+$r] [expr $y0+$r] -fill $color -outline $color
+        $c create oval [expr $x0-$r] [expr $y0-$r] [expr $x0+$r] [expr $y0+$r] -fill $color -outline $color
         set x1 [expr $x0+$w/12.+1]
         set r [expr $r/1.25]
-        $c create oval [expr $x1-$r] [expr $y0-$r] \
-            [expr $x1+$r] [expr $y0+$r] -fill $bg -outline $bg
+        $c create oval [expr $x1-$r] [expr $y0-$r] [expr $x1+$r] [expr $y0+$r] -fill $bg -outline $bg
     }
  }
 
