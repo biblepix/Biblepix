fdf3cd3461ac9136403466734714a79fbe0dc4ef
linuxpeter <linux@vollmar.ch>
2017-09-27 14:14:57 +0200

    Syntax aufgeräumt!

--- prog/src/save/setupSave.tcl
+++ prog/src/save/setupSave.tcl
@@ -2,135 +2,125 @@
 # Records settings & downloads TWD files
 # called by biblepix-setup.tcl
 # Author: Peter Vollmar & Joel Hochreutener, biblepix.vollmar.ch
-# Updated : 26sep17 
+# Updated : 27sep17 
 
-# TODO - remove, this is handled via Download button now
-#Make sure either $twdDir or SELECTED contain $jahr-TWD files,
-# else stop saving process & return to Setup!
-#set SELECTED_TWD_FILES [.n.f1.twdremoteframe.lb curselection]
-# A: If SELECTED NOT EMPTY: Start TWD download
-#if { $SELECTED_TWD_FILES != ""} {
-#  downloadTWDFiles
-#}
-
-# return to INTERNATIONAL section if $twdDir empty
+# Return to INTERNATIONAL section if $twdDir empty
 if { [catch {glob $twdDir/*$jahr.twd}] } {
-    
   .n select .n.f1
   NewsHandler::QueryNews "$noTWDFilesFound" red
+  return 1
 
-#return to PHOTOS section if $picsdir empty
+# Return to PHOTOS section if $picsdir empty
 } elseif { [catch {glob $jpegDir/*}] } {
- 
-  .n select .n.f6
+ .n select .n.f6
   NewsHandler::QueryNews "$noPhotosFound" red
+  return 1
+}
 
-# else continue with writing Config
+
+# R E W R I T E   C O N F I G
+
+#1.Fetch status variables from GUI
+set imgstatus [set imgyesState]
+set sigstatus [set sigyesState]
+set introlinestatus [set enableintro]
+set fontcolourstatus [$fontcolorSpin get]
+set fontsizestatus [$fontsizeSpin get]
+set fontweightstatus [set fontweightState]
+set fontfamilystatus [$fontfamilySpin get]
+
+if {$fontweightstatus==1} {
+  set fontweight "bold"
 } else {
-
-  #2. R E W R I T E   C O N F I G
-
-  #Fetch status variables
-  set imgstatus [set imgyesState]
-  set sigstatus [set sigyesState]
-  set introlinestatus [set enableintro]
-  catch {set termstatus [set termyesnoState]}
-  set fontcolourstatus [$fontcolorSpin get]
-  set fontsizestatus [$fontsizeSpin get]
-  set fontweightstatus [set fontweightState]
-  set fontfamilystatus [$fontfamilySpin get]
-  set slidestatus [$slideSpin get]
+  set fontweight "normal"
+}
   
-  #Fetch textpos coordinates
-  lassign [$textposCanv coords mv] x y - -
-  set marginleftstatus [expr int($x*$textPosFactor)]
-  set margintopstatus [expr int($y*$textPosFactor)]
+set fontcolor [set $fontcolourstatus]
+set rgb [hex2rgb $fontcolor]
+set slidestatus [$slideSpin get]
 
-  #Write all settings to config
-  set chan [open $Config w]
-  puts $chan "set lang $lang"
-  puts $chan "set enableintro $introlinestatus"
-  puts $chan "set enablepic $imgstatus"
-  puts $chan "set enablesig $sigstatus"
+##textpos coordinates
+lassign [$textposCanv coords mv] x y - -
+set marginleftstatus [expr int($x*$textPosFactor)]
+set margintopstatus [expr int($y*$textPosFactor)]
+
+##Linux specific
+catch {set termstatus [set termyesnoState]}
+
+#2.Write general vars to $Config
+set chan [open $Config w]
+puts $chan "set lang $lang"
+puts $chan "set enableintro $introlinestatus"
+puts $chan "set enablepic $imgstatus"
+puts $chan "set enablesig $sigstatus"
+puts $chan "set slideshow $slidestatus"
+puts $chan "set fontfamily \{$fontfamilystatus\}"
+puts $chan "set fontsize $fontsizestatus"
+puts $chan "set fontcolortext $fontcolourstatus"
+puts $chan "set fontweight $fontweight"
+puts $chan "set sun [setSun $rgb]"
+puts $chan "set shade [setShade $rgb]"
+puts $chan "set marginleft $marginleftstatus"
+puts $chan "set margintop $margintopstatus"
+
+#3.Write Linux vars to $Config
+if {$os == "Linux"} {
+  if {[info exists crontab]} {
+    puts $chan "set crontab 1"
+  }
   if {[info exists termstatus]} {
     puts $chan "set enableterm $termstatus"
   }
-  puts $chan "set slideshow $slidestatus"
-  puts $chan "set fontfamily \{$fontfamilystatus\}"
-  puts $chan "set fontsize $fontsizestatus"
+} 
 
-  if {$fontweightstatus==1} {
-    puts $chan "set fontweight bold"
-  } else {
-    puts $chan "set fontweight normal"
-  }
-  puts $chan "set fontcolortext $fontcolourstatus"
+close $chan
 
-  ##Compute sun & shade HEX - (procs in Imgtools!)
-  set fontcolor [set $fontcolourstatus]
-  set rgb [hex2rgb $fontcolor]
-  puts $chan "set sun [setSun $rgb]"
-  puts $chan "set shade [setShade $rgb]"
-  puts $chan "set marginleft $marginleftstatus"
-  puts $chan "set margintop $margintopstatus"
+
+#F I N I S H
+NewsHandler::QueryNews "Changes recorded. Exiting..." green
+#leere das gesamte Fenster, weil es wiederverwendet wird.
+pack forget .n .fbottom .ftop
+
+#R U N   I N S T A L L   R O U T I N E S
+
+# 1. put biblepix-setup.tcl & biblepix.tcl in Desktop program menu
+# 2. put biblepix.tcl in Autostart
+# 3. set up Desktop background image & slide show
+
+source $Config
+
+if {$os == "Windows NT"} {
+  source $SetupSaveWin
+} elseif {$os == "Linux"} {
+  source $SetupSaveLin
+}
+   
+#Delete any old TWD files - TODO : isn't there a better place for this?
+set vorjahr [expr {$jahr - 1}]
+set oldtwdlist [glob -nocomplain -directory $twdDir *$vorjahr.twd]
+if {[info exists oldtwdlist]} {
+  NewsHandler::QueryNews "Deleting old language files..." lightblue
   
-  close $chan
-
-  #Finish
-  NewsHandler::QueryNews "Changes recorded. Exiting..." green
-
-  #leere das gesamte Fenster, weil es wiederverwendet wird.
-  pack forget .n .fbottom .ftop
-
-  #######  I N S T A L L   R O U T I N E S   WIN / LINUX / MAC
-
-  # 1. puts biblepix-setup.tcl & biblepix.tcl in Desktop program menu
-  # 2. puts biblepix.tcl in Autostart
-  # 3. sets Desktop background image & slide show
-
-  if {$os == "Windows NT"} {
-    
-    source $Config
-    source $SetupSaveWin
-
-  } elseif {$os == "Linux"} {
-    
-    source $Config
-    source $SetupSaveLin
-    if {[info exists crontab]} {
-      set chan [open $Config a]      
-      puts $chan "set crontab 1"
-      close $chan
-    }   
+  foreach file $oldtwdlist {
+    file delete $file
   }
   
-  #Delete any old TWD files - TODO : isn't there a better place for this?
-  set vorjahr [expr {$jahr - 1}]
-  set oldtwdlist [glob -nocomplain -directory $twdDir *$vorjahr.twd]
-  if {[info exists oldtwdlist]} {
-    NewsHandler::QueryNews "Deleting old language files..." lightblue
-    
-    foreach file $oldtwdlist {
-      file delete $file
-    }
-    
-    NewsHandler::QueryNews "Old TWD files deleted." green
+  NewsHandler::QueryNews "Old TWD files deleted." green
+}
+
+#Delete old BMPs & start Biblepix
+if {$enablepic} {
+  #create random BMP if $imgDir empty
+  if { [glob -nocomplain $imgDir/*.bmp] == "" } {
+    package require Img
+    set photopath [getRandomPhoto]
+    set quickimg [image create photo -file $photopath]
+    $quickimg write $TwdBMP -format bmp
+  }
+  foreach file [glob -nocomplain -directory $bmpdir *] {
+    file delete -force $file
   }
 
-  #Delete old BMPs & start Biblepix
-  if {$enablepic} {
-    #create random BMP if $imgDir empty
-    if { [glob -nocomplain $imgDir/*.bmp] == "" } {
-      package require Img
-      set photopath [getRandomPhoto]
-      set quickimg [image create photo -file $photopath]
-      $quickimg write $TwdBMP -format bmp
-    }
-    foreach file [glob -nocomplain -directory $bmpdir *] {
-      file delete -force $file
-    }
+}
 
-  }
-  
-  source $Biblepix
-} ;#END WRITE CONFIG
+source $Biblepix
