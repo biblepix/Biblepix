d8d414ead405b858f45c8bc95cd2d011190d9bcb
linuxpeter <linux@vollmar.ch>
2018-07-17 12:11:21 +0200

    bugfix

--- prog/src/save/setupSaveLinHelpers.tcl
+++ prog/src/save/setupSaveLinHelpers.tcl
@@ -308,7 +308,7 @@ Comment=Runs & configures BiblePix"
   return 0
 } ;#END setLinMenu
 
-# setKdeActionMenu
+# setupKdeActionMenu
 ## Produces right-click action menu in Konqueror (and possibly Dolphin?)
 ## seen to work only in some versions of KDE5 - very buggy!
 ## Called by SetupSaveLin ?if KDE detected?
@@ -358,7 +358,7 @@ Exec=$Setup
 # B A C K G R O U N D   P I C   S E T T E R S
 ################################################
 
-# setKdeBackground
+# setupKdeBackground
 ## Configures KDE4 or KDE5 Plasma for single pic or slideshow
 # TODO: > Anleitung in Manpage für andere KDE-Versionen/andere Desktops (Rechtsklick > Desktop-Einstellungen >Einzelbild/Diaschau)
 
@@ -398,7 +398,7 @@ proc setupKdeBackground {} {
   
 } ;#END setKdeBackground
 
-# setKde4Bg
+# setupKde4Bg
 # called by setKdeBackground if KDE4 rcfile found
 proc setupKde4Bg {kread kwrite} {
   global slideshow
@@ -432,7 +432,7 @@ proc setupKde4Bg {kread kwrite} {
   return 0
 } ;#END setKde4Bg
 
-# setKde5Bg
+# setupKde5Bg
 ## called by setKdeBackground if KDE5 found
 ## expects rcfile [file join $env(HOME) .config plasma-org.kde.plasma.desktop-appletsrc]
 ## expects correct version of kreadconfig(?5) kwriteconfig(?5)
@@ -467,7 +467,7 @@ proc setupKde5Bg {rcfile kread kwrite} {
   return 0
 } ;#END setKde5Bg
 
-# setXfceBackground
+# setupXfceBackground
 ##configures XFCE4 single pic or slideshow - TODO: >update MANPAGE!!!!!!!!!
 ### configFile hierarchy: 
   #<channel name="xfce4-desktop" ...>
@@ -475,7 +475,121 @@ proc setupKde5Bg {rcfile kread kwrite} {
   #  <property name="screen0" type="empty">
   #    <property name="monitor0" type="empty">
   #      <property name="image-path"..."/>
+#xfconf-query syntax für neue 'property':
+# xfconf-query -c -p* -n -t -s
+# * ganzer (neuer) Pfad
+##########################################
+
+#NEW ATTEMPT WITH native tools!!!!
 proc setupXfceBackground {} {
+  global slideshow TwdBMP TwdTIF
+  
+  #Exit if xfconf-query not found
+  if {[auto_execok xfconf-query] == ""} {
+    return 1
+  }
+  
+  #Our 'channel' is actually an XML file found in .config/xfce4/xfconf/xfce-perchannel-xml/
+  set channel "xfce4-desktop"
+  
+  puts "Configuring XFCE background image..."
+
+proc meyutar {} {
+  #TODO: Check if this is really needed !!!!!!!!!!!!!!
+  #was dropped back in 2015 !!!!!!!!!!!!!¨
+  #Create/Change backdrop.list if $slideshow
+  if {$slideshow} {
+    set backdropdir ~/.config/xfce4/desktop
+    file mkdir $backdropdir
+    set backdroplist $backdropdir/backdrop.list
+    set chan [open $backdroplist w]
+    puts $chan "$TwdBMP\n$TwdTIF"
+    close $chan
+  }
+}
+
+ #Set monitoring - no Luck, holds up everything!
+#exec xfconf-query -c xfce4-desktop -m
+  
+#xfconf-query -c xfce4-desktop -l >
+#/backdrop/screen0/monitor0/image-path NEEDED [path]
+#/backdrop/screen0/monitor0/workspace0/backdrop-cycle-enable NEEDED true
+#/backdrop/screen0/monitor0/workspace0/backdrop-cycle-timer NEEDED int
+
+#Check monitor name
+set desktopXmlTree [exec xfconf-query -c xfce4-desktop -l]
+if [regexp {monitor[0-9]} $desktopXmlTree] {
+  set monitorName "monitor"
+} else {
+  regexp -line {(backdrop/screen0/)(.*)(/.*$)} $t var1 monitorName var3
+  set imgpath /backdrop/screen$s/$monitorName$m/image-path
+      set imgStylePath /backdrop/screen$s/$monitorName$m/image-style
+      exec xfconf-query -c $channel -p $imgpath -n -t string -s $TwdBMP
+        exec xfconf-query -c $channel -p $imgStylePath -n -t int -s 3
+}
+
+  #Scan through 4 screeens & monitors
+  for {set s 0} {$s<5} {incr s} {
+    for {set m 0} {$m<5} {incr m} {
+    
+      # 'set' = set if existent
+      # 'create' = create if non-existent
+
+      set imgpath /backdrop/screen$s/$monitorName$m/image-path
+      set imgStylePath /backdrop/screen$s/$monitorName$m/image-style
+      if [catch "exec xfconf-query -c $channel -p $imgpath"] {
+      
+        continue
+      
+      } else {
+      
+        puts "Setting $imgpath"
+      
+        #must set single img path even if slideshow!
+        exec xfconf-query -c $channel -p $imgpath -n -t string -s $TwdBMP
+        exec xfconf-query -c $channel -p $imgStylePath -n -t int -s 3
+        set ctrlBit 1
+      }
+
+      if {$slideshow} {
+        
+        #run through 4 workspaces (w) 
+        #NOTE: any number of ws's can be added, but standard is 4.
+        for {set w 0} {$w<4} {incr w} {
+        puts "Setting workspace $w"
+        
+        #set cycle-timer in secs, set cycle-period to secs (=0), set type to 'uint'
+          set backdropCycleEnablePath /backdrop/screen$s/$monitorName$m/workspace$w/backdrop-cycle-enable
+          set backdropCycleTimerPath /backdrop/screen$s/$monitorName$m/workspace$w/backdrop-cycle-timer
+          set backdropCycleTimerPeriod /backdrop/screen$s/$monitorName$m/workspace$w/backdrop-cycle-period
+          exec xfconf-query -c $channel -p $backdropCycleEnablePath -n -t bool -s true
+          exec xfconf-query -c $channel -p $backdropCycleTimerPath -n -t uint -s $slideshow
+          exec xfconf-query -c $channel -p $backdropCycleTimerPeriod -n -t int -s 0
+          
+        } ;#END for3
+      } ;#END if slideshow
+    } ;#END for2
+  } ;#END for1
+    
+
+#reload XFCE4 desktop if running - TODO hammer des net scho woanders?
+proc reloadXfceDesktop {} {
+  if {! [catch "exec pidof xfdesktop"] } {
+    wm withdraw .
+    exec xfdesktop --reload
+  }
+}
+
+  if [info exists ctrlBit] {
+      return 0
+  } {
+    puts NoLuckSettingXfce
+    return 1
+  }
+  
+} ;#END setXfceBackground
+ 
+proc setupXfceBackgroundOLD {} {
   global slideshow Xfce4ConfigFile LinConfDir
   package require tdom
  
@@ -577,7 +691,7 @@ puts finishedChangingXFCEConfFile
 } ;#END setXfceBackground
 
 
-# setGnomeBackground - TODO: das funktioniert nicht mit return!
+# setupGnomeBackground - TODO: das funktioniert nicht mit return!
 ##configures Gnome single pic
 ##setting up slideshow not needed because Gnome detects picture change automatically
 proc setupGnomeBackground {} {
