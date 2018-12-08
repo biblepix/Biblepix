bc16df4086bc04cad969216be9014a3dfef5c81e
Joel Hochreutener <cjhox9@gmail.com>
2018-12-08 18:13:06 +0100

    fixes

--- prog/src/updateInjection.tcl
+++ prog/src/updateInjection.tcl
@@ -2,7 +2,7 @@
 # Regulates shift vom version 2.4 to 3.0
 # sourced once by biblepix-setup.tcl
 # Authors: Peter Vollmar & Joel Hochreutener, biblepix.vollmar.ch
-# Updated: 3dec18
+# Updated: 8dec18
 
 #Return to Setup if wrong version
 if {$version == "2.4"} {
@@ -27,43 +27,53 @@ if {$version == "2.4"} {
   sourceHTTP
   runHTTP 1
   source $Globals
-
-  # 2 Run SaveWin/SaveLin to install new Setup path
-  if {$os=="Linux"} {
-    source $SaveLinHelpers
-    setupLinMenu
-  } else {
-    source $SaveWinHelpers
-    setWinContextMenu
-  }
+  
+  # 2 Move twd files
+  file rename $twdDir [file join $rootdir BibleTexts]
 
   # 3 Delete obsolete directories in rootdir & $srcdir
+  set oldDirList ""
+  foreach path [glob -directory $rootdir -type d *] {
+    lappend oldDirList $path
+  }
+  foreach path [glob -directory $srcdir -type d *] {
+    lappend oldDirList $path
+  }
 
-  ##set current directories list
-  lappend oldDirList [file normalize [glob -directory $rootdir -type d *]]
-  lappend oldDirList [file normalize [glob -directory $srcdir -type d *]]
   ##set new directories list for $rootdir
-  lappend newDirList [file normalize [file join $rootdir prog]]
-  lappend newDirList [file normalize [file join $rootdir TodaysSignature]]
-  lappend newDirList [file normalize [file join $rootdir TodaysPicture]]
-  lappend newDirList [file normalize [file join $rootdir Photos]]
-  lappend newDirList [file normalize [file join $rootdir BibleTexts]]
-  lappend newDirList [file normalize [file join $rootdir Docs]]
+  set newDirList ""
+  lappend newDirList [file join $rootdir prog]
+  lappend newDirList [file join $rootdir TodaysSignature]
+  lappend newDirList [file join $rootdir TodaysPicture]
+  lappend newDirList [file join $rootdir Photos]
+  lappend newDirList [file join $rootdir BibleTexts]
+  lappend newDirList [file join $rootdir Docs]
   ##set new directories list for $srcdir
-  lappend newDirList [file normalize [file join $srcdir share]]
-  lappend newDirList [file normalize [file join $srcdir setup]]
-  lappend newDirList [file normalize [file join $srcdir sig]]
-  lappend newDirList [file normalize [file join $srcdir pic]]
-  lappend newDirList [file normalize [file join $srcdir save]]
+  lappend newDirList [file join $srcdir share]
+  lappend newDirList [file join $srcdir setup]
+  lappend newDirList [file join $srcdir sig]
+  lappend newDirList [file join $srcdir pic]
+  lappend newDirList [file join $srcdir save]
+
   ##delete obsolete dir paths including all files
-  foreach dirpath $oldDirList {
-    if { ![string match *$dirpath* $newDirList]} {
-      file delete -force $dirpath
+  foreach oldDirPath $oldDirList {
+    puts $oldDirPath
+    set keep 0
+    foreach newDirPath $newDirList {
+      if { $oldDirPath == $newDirPath } {
+        set keep 1
+        puts "keep"
+        break
+      }
+    }
+    if { !$keep } {
+      puts "delete"
+      file delete -force $oldDirPath
     }
   }
+
   ##delete obsolete single files
   file delete $rootdir/README
-  file delete $srcdir/biblepix-setup.tcl
   file delete $picdir/hgbild.tcl
   file delete $picdir/textbild.tcl
 
