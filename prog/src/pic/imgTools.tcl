cb2ad60fd6fa8763d4b1ca9797e7e6f59590deeb
Joel Hochreutener <cjhox9@gmail.com>
2020-04-22 14:23:15 +0200

    fix git problem

--- prog/src/pic/imgTools.tcl
+++ prog/src/pic/imgTools.tcl
@@ -174,7 +174,7 @@ proc doResize {canv} {
   set origX [image width photosOrigPic]
   set origY [image height photosOrigPic]
   
-  #TODO besser mit winfo, falls fenster überdeckt ...
+#TODO besser mit winfo, falls fenster überdeckt ...
   set canvX [lindex [$canv conf -width] end]
   set canvY [lindex [$canv conf -height] end]
   lassign [$canv bbox img] canvPicX1 canvPicY1 canvPicX2 canvPicY2
@@ -194,6 +194,7 @@ proc doResize {canv} {
   ResizeHandler::QueryResize $cutImg
   after idle {
     ResizeHandler::Run
+    #openReposWin
   }
 }
 
@@ -206,14 +207,19 @@ proc processResize {cutImg} {
   set screenX [winfo screenwidth .]
   set screenY [winfo screenheight .]
 
-  NewsHandler::QueryNews "$::resizingPic" orange  
-  set finalImage [resizePic $cutImg $screenX $screenY]
-  
-#  set targetPicPath [file join $dirlist(photosDir) [setPngFileName [file tail $picPath]]]
-#  $finalImage write $targetPicPath -format PNG
+  NewsHandler::QueryNews "$::resizingPic" orange
 
-  image delete $cutImg
-  image delete $finalImage
+
+#TODO vorläufig bleibt's in cutOrigPic - no saving!  
+  set finalImage [resizePic $cutImg $screenX $screenY]
+  image create photo cutOrigPic
+  #resizePic $cutImg $screenX $screenY cutOrigPic
+
+  set targetPicPath [file join $dirlist(photosDir) [setPngFileName [file tail $picPath]]]
+  $finalImage write $targetPicPath -format PNG
+
+#  image delete $cutImg
+#image delete $finalImage
 
   NewsHandler::QueryNews "[copiedPicMsg $picPath]" lightblue
 
@@ -414,6 +420,7 @@ proc resizePic {src newx newy {dest ""} } {
     incr ny
   }
   update
-
+  
+  puts $dest
   return $dest
 } ;#END resizePic
