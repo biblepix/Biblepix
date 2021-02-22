33b0750c247e594338ed2dbd874e70e7223bbad9
Biblepix <biblepix@vollmar.ch>
2021-02-22 21:15:31 +0000

    restored

--- prog/unix/term.sh
+++ prog/unix/term.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 # ~/Biblepix/prog/unix/term.sh
 # Bash script to display 'The Word' in a Linux terminal
-# Recreated on 15May2019 at 12:07
+# Recreated on 02Jan2021 at 17:06
 
 . /home/pv/Biblepix/prog/conf/term.conf
-echo -e ${titbg}${tit}"* An Briathar do Dé Céadaoin, 15 Bealtaine, 2019 *"
-echo -e ${txt}"     Is follas iad gníomhartha na colainne,"
-echo -e ${txt}"     mar atá drúis gáirsiúlacht agus graostacht;"
-echo -e ${txt}"     íoladhradh agus asarlaíocht; eascairdeas, achrann, agus formad;"
-echo -e ${txt}"     fearg, bruíonta, clampar agus faicsin, éad, (murdail, )"
-echo -e ${txt}"     meisce, ragairne, agus a leithéid eile."
-echo -e ${ref}${tab}"Galataigh 5:19-21"
-echo -e ${txtrst}${int}"     Dúirt an Tiarna le Cháin:"
-echo -e ${txt}"     Mura ndéana tú an mhaith,"
-echo -e ${txt}"     nach shin é an peaca ar an tairseach agat"
-echo -e ${txt}"     agus craos air chugat ach go gcaithfir é a cheansú?"
-echo -e ${ref}${tab}"Geineasas 4:7"
+echo -e ${titbg}${tit}"* La Palabra para sábado, 2 enero 2021 *"
+echo -e ${txt}"     En los cielos está mi testigo"
+echo -e ${txt}"     y mi testimonio en las alturas."
+echo -e ${ref}${tab}"Job 16,19"
+echo -e ${txt}"     Cuando mi alma desfallecía en mí,"
+echo -e ${txt}"     me acordé de Jehová,"
+echo -e ${txt}"     y mi oración llegó hasta ti,"
+echo -e ${txt}"     hasta tu santo templo."
+echo -e ${ref}${tab}"Jonás 2,7"
 tclsh /home/pv/Biblepix/prog/src/term/terminal.tcl &
