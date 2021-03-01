ee8bf1985ce3e7908784837e7801b30bd1e037ee
Biblepix <biblepix@vollmar.ch>
2021-03-01 09:40:31 +0000

    WIP

--- prog/src/share/globals.tcl
+++ prog/src/share/globals.tcl
@@ -191,8 +191,6 @@ array set BlueArr {r 30 g 100 b 244}
 array set BrownArr {r 150 g 40 b 40}
 array set GoldArr  {r 255 g 215 b 0}
 
-#array set GreenArr {r 60 g 185 b 120}
-#10% dunkler:
 array set GreenArr {r 54 g 167 b 108}
 
 array set SilverArr {r 160 g 160 b 160}
