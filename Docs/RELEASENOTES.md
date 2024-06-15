7522c3226c32166250013ade0c727923f538a46f
Biblepix <biblepix@vollmar.ch>
2024-06-15 14:52:45 +0200

    final bugfixes & layout fixes

--- Docs/RELEASENOTES.md
+++ Docs/RELEASENOTES.md
@@ -1,32 +1,19 @@
 # BiblePix Release Notes
 
-## Version 4.2 Top Features
+## Version 5.0 Top Features
 
 - Setup program runs in 10 languages
 - 5 font sizes for all languages (added 12pt)
 - Automatic program update in background
-- New colour management for readability of text
 
-## Shift from version 3.1 to 4.0
+## Shift from version 4.1 to 5.0
 
 ### Fixed bugs
 
-Arabic & Hebrew display now faultless (fully vowelled text in the Setup, devowelled on the photo for readability)
+quite a number of minor bugs
 
 ### Added features
 
-- Thai and Chinese fonts now in all sizes and variants
-- Automatic program update in the background, whenever BiblePix is run (this used to happen only when running Setup)
-- Setup (International): list of Bible texts for download now in English and native languages
-- Setup (Desktop): Choosable text colours for Desktop picture now: Gold/Silver/Leaf/Sea/Slate with an optional "accidental" switch (colour is chosen at random for each new picture)
-- Setup (Photos): Number of installed pictures now shown
-- Setup (Photos): Rotating function added for badly taken photos
-- Setup (Photos): Text position can now be set for each individual picture when adding to the BiblePix photo collection
-- Setup (Photos): Ill-dimensioned pictures can be fitted to screen dimensions before saving
-- Setup (e-mail): desired language(s) for mail signatures now choosable
+- Setup (Photos): Open File Dialog: Number of (installed) pictures per directory shown
+- Setup (Photos): Photo navigation now easier and clearer
 - Setup (Photos): faster image loading through implementation of threads
-
-### New colour management
-
-To enhance readability, text colour brightness is automatically adjusted depending on the brightness of the text area.
-This information is saved along with the picture, as well as any positioning information (if desired, see above) when adding a photo to the BiblePix photo collection.
--- prog/src/setup/setupInternational.tcl
+++ prog/src/setup/setupInternational.tcl
@@ -1,7 +1,7 @@
 # ~/Biblepix/prog/src/setup/setupEmail.tcl
 # Sourced by setupBuildGUI
 # Authors: Peter Vollmar & Joel Hochreutener, biblepix.vollmar.ch
-# Updated 3jan23 pv
+# Updated 15june24 pv
 
 #Create Title & msg in main frame
 label .intTitleL -textvar msg::f1Tit -font bpfont3
@@ -11,17 +11,16 @@ pack .intTitleL .intTxtM -in .internationalF -side top -anchor w
 #Create frames
 pack [frame .intTopF -padx $px] -in .internationalF -anchor w -fill x -expand 1
 pack [frame .intMidF -padx $px] -in .internationalF -anchor w -fill x
-pack [frame .intBotF -padx $px] -in .internationalF -anchor w -fill x
-
+pack [frame .intBotF -padx $px] -in .internationalF -anchor w -fill none
 
 #Refresh button
-button .intRefreshBtn -textvar msg::refresh -bg orange -activebackground orange -command {
+button .intRefreshBtn -textvar msg::refresh -bd 2 -activebackground orange -command {
   NewsHandler::QueryNews "[getRemoteTWDFileList]" orange
 }
-pack .intRefreshBtn -in .intBotF -side bottom -fill x -padx $px
+pack .intRefreshBtn -in .intBotF -side bottom -padx $px -pady 3
 
 ##subframes
-pack [frame .twdremoteTitleF -bg beige] -in .intBotF -side top -fill x -anchor w -padx $px
+pack [frame .twdremoteTitleF] -in .intBotF -side top -fill x -anchor w -padx $px -pady 5 
 pack [frame .twdremoteF -padx $px] -in .intBotF
 label .intStatusL -textvar status -font bpfont1 -height 1 -bg $bg -relief sunken
 pack .intStatusL -in .internationalF -fill x
@@ -30,7 +29,7 @@ pack .intStatusL -in .internationalF -fill x
 label .intTwdlocalTit -textvar msg::TwdLocalTit -font bpfont2
 pack .intTwdlocalTit -in .intTopF -side top -anchor w -fill x
 ##create local listbox & scrollbar
-set localLB [listbox .intTwdlocalLB -bg lightgreen -width [expr $tw - $px] -selectmode single -activestyle none -yscrollcommand {.intTwdlocalSB set}]
+set localLB [listbox .intTwdlocalLB -bg beige -width [expr $tw - $px] -selectmode single -activestyle none -yscrollcommand {.intTwdlocalSB set} -bd 2]
 scrollbar .intTwdlocalSB -command {$localLB yview}
 ##fill listbox
 set twdlist [getTwdList]
@@ -47,26 +46,26 @@ button .intDelBtn -bg $bg -textvar msg::delete -command {
   }
   updateMailBtnList .mailTop2F
 }
-pack .intDelBtn -in .intTopF -side right 
+pack .intDelBtn -in .intTopF -side right -padx 3 
 pack .intTwdlocalSB -in .intTopF -side right -fill y
 pack $localLB -in .intTopF -side left -padx $px
 
 # R E M O T E   T W D   l i s t 
-label .intTwdremoteTit -textvar msg::TwdRemoteTit -justify left -font bpfont2 -padx $px -pady $py
+label .intTwdremoteTit -textvar msg::TwdRemoteTit -justify left -font bpfont2 -padx $px -pady 3
 pack .intTwdremoteTit -in .intMidF -side top -fill x
 ##Titel frame
-label .twdremote1L -font "SmallCaptionFont 8" -textvar msg::language -font TkFixedFont -bg beige -anchor w -width 20
-label .twdremote2L -font "SmallCaptionFont 8" -textvar msg::year -font TkFixedFont -bg beige -anchor w -width 14
-label .twdremote3L -font "SmallCaptionFont 8" -textvar msg::biblename -font TkFixedFont -bg beige -anchor w -width 59
-label .twdremote4L -font "SmallCaptionFont 8" -textvar msg::bibleversion -font TkFixedFont -bg beige -anchor w
+label .twdremote1L -font "TkCaptionFont" -textvar msg::language -anchor w -width 18
+label .twdremote2L -font "TkCaptionFont" -textvar msg::year -anchor w -width 12
+label .twdremote3L -font "TkCaptionFont" -textvar msg::biblename -anchor w -width 52
+label .twdremote4L -font "TkCaptionFont" -textvar msg::bibleversion -anchor w
 pack .twdremote1L .twdremote2L .twdremote3L .twdremote4L -in .twdremoteTitleF -side left
 #Create remote listbox & scrollbar
 ##content inserted later by http.tcl
-listbox .twdremoteLB -yscrollcommand {.twdremoteSB set} -selectmode multiple -activestyle none -font TkFixedFont -width [expr $wWidth - 50] -height [expr $wHeight - 300] -bg lightblue
+listbox .twdremoteLB -yscrollcommand {.twdremoteSB set} -selectmode multiple -activestyle none -font TkFixedFont -width [expr $wWidth - 50] -height [expr $wHeight - 300] -bg grey90 -bd 2
 scrollbar .twdremoteSB -command {.twdremoteLB yview}
 button .downloadBtn -textvar msg::download -command {
   downloadTWDFiles
   catch {updateMailBtnList .mailTop2F}
 }
-pack .downloadBtn -in .twdremoteF -side right -fill x
+pack .downloadBtn -in .twdremoteF -side right -padx 3
 pack .twdremoteSB .twdremoteLB -in .twdremoteF -side right -fill y
