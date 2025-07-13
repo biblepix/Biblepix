a74f2b80b9323f3e446efbce36dc9cb7eeba5b0d
Biblepix <biblepix@vollmar.ch>
2025-07-13 15:26:23 +0000

    trying to set moving by arrow keys - no luck

--- prog/src/setup/setupResizePhoto.tcl
+++ prog/src/setup/setupResizePhoto.tcl
@@ -86,19 +86,19 @@ set bildname $canvpic::thumb
 #frame $w.topF -bg beige
 #frame $w.botF -bg yellow
 #pack $w.topF $w.botF
-#Create scrollbars - TODO NOT WOWRKING!
-  scrollbar $w.resizeRightSB -orient vertical -command {$resizepic::c yview} 
-	scrollbar $w.resizeBotSB -orient horizontal -command {$resizepic::c xview}
-	$resizepic::c conf -yscrollcommand "$w.resizeRightSB set" -xscrollcommand "$w.resizeBotSB set"
+#Create scrollbars - TODO Delete or Fix (move content of canvas not canvas itself)
+ # scrollbar $w.resizeRightSB -orient vertical -command {$resizepic::c yview} 
+#	scrollbar $w.resizeBotSB -orient horizontal -command {$resizepic::c xview}
+#	$resizepic::c conf -yscrollcommand "$w.resizeRightSB set" -xscrollcommand "$w.resizeBotSB set"
 
 	#Create progress bar
 	ttk::progressbar $w.pb -mode indeterminate -length 150
 
 
 pack $w.pb -pady 15 -side bottom
-pack $w.resizeBotSB -side bottom -fill x
+#pack $w.resizeBotSB -side bottom -fill x
 pack $resizepic::c -side left
-pack $w.resizeRightSB -side right -fill y
+#pack $w.resizeRightSB -side right -fill y
 
 	
   #Set bindings
@@ -113,13 +113,15 @@ pack $w.resizeRightSB -side right -fill y
 #TODO Joel: try to bind movements to Mousewheel + Arrow keys:
 ## But how to define specific steps?????
 #$resizepic::c bind mv <MouseWheel> {%W yview scroll [expr {- (%D)}] units}
-set moveUD {%W yview scroll [expr {-%D/120}] units}
+#set moveUD {%W yview scroll [expr {-%D/120}] units}
+set moveUD {%w yview scroll [expr {-%D/120}] units}
 set moveRL {%W xview scroll [expr {-%D/120}] units}
 
 bind $resizepic::c <MouseWheel> $moveUD
 
-#  $resizepic::c bind mv <Key-Up> $moveUD
-  bind $resizepic::c <Key-uparrow> $cmd
+#TODO not working
+$resizepic::c bind {mv,img} <Key-uparrow> {%W yview scroll 1 "units"}
+ # bind $resizepic::c <Key-uparrow> $cmd
 #  $repospic::canv bind mv <Down>
 #  $repospic::canv bind mv <Right>
 #  $repospic::canv bind mv <Left>
@@ -140,20 +142,34 @@ proc openReposWindow {pic} {
   catch {destroy .resizePhoto}
   global fontsize fontcolortext
   namespace eval repospic {}
+  
+  set margin 10
 
-  set repospic::reposCanvPic [image create photo]
-  set repospic::w [toplevel .reposPhoto -bg lightblue -padx 20 -pady 20 -height 400 -width 600]
-  after idle {tk::PlaceWindow .reposPhoto center}
-  
-  set repospic::canv [canvas $repospic::w.reposCanv -bg lightblue]
-  
-  $repospic::canv create image 0 0 -image $pic -anchor nw -tags {img mv}
-  pack $repospic::canv
+  set origW [image width $pic]
+  set origH [image height $pic]
+ puts "orig: $origW $origH"
 
   image create photo reposCanvPic
  
-	set repospic::scaleFactor [getReposScalefactor $pic]
-	reposCanvPic copy $pic -subsample $repospic::scaleFactor
+	set scaleFactor 2
+	reposCanvPic copy $pic -subsample $scaleFactor
+  reposCanvPic conf -width [expr $origW / $scaleFactor] -height [expr $origH / $scaleFactor]
+  
+  set canvX [image width reposCanvPic]
+  set canvY [image height reposCanvPic]
+ puts "canv: $canvX $canvY"
+  
+  set winX [expr $canvX + 2*$margin]
+  set winY [expr $canvY + 2*$margin]
+ puts "win: $winX $winY"
+  
+  set repospic::w [toplevel .reposPhoto -bg lightblue -padx $margin -pady $margin -height $winY -width $winX]
+  after idle {tk::PlaceWindow .reposPhoto center}
+  
+  set repospic::canv [canvas $repospic::w.reposCanv -bg lightblue -height $canvY -width $canvX]
+  
+  $repospic::canv create image 0 0 -image reposCanvPic -anchor nw -tags {img mv}
+  pack $repospic::canv
 
   #Define button actions
   set cancelBtnAction {
@@ -171,8 +187,8 @@ proc openReposWindow {pic} {
     setCanvasFontColour $repospic::canv $fontcolortext $lum
     #Process PNG info
     lassign [$repospic::canv coords txt] x y
-    set x [expr $x * $repospic::scaleFactor]
-    set y [expr $y * $repospic::scaleFactor]
+    set x [expr $x * $scaleFactor]
+    set y [expr $y * $scaleFactor]
     processPngComment $addpicture::targetPicPath $x $y $lum
 
     NewsHandler::QueryNews "$msg::reposSaved" lightgreen
@@ -190,25 +206,14 @@ proc openReposWindow {pic} {
      set ::y %Y
   }
   $repospic::canv bind mv <B1-Motion> {dragCanvasItem %W txt %X %Y 20}
-  
-#  set imgX [image width $repospic::reposCanvPic]
-#  set imgY [image height $repospic::reposCanvPic]
-  set imgX [image width reposCanvPic]
-  set imgY [image width reposCanvPic]
-  
-  $repospic::canv conf -width $imgX -height $imgY
 
   #Creaste moving text (with positon & luminance)
   createMovingTextBox $repospic::canv
   
   #Configure text size
    set screenX [winfo screenwidth .]
-   set fontfactor [expr $screenX / $imgX]
-   if !$fontfactor {
-     set canvFontsize $::fontsize
-   } {
-     set canvFontsize [expr round($::fontsize / $fontfactor)]
-   }
+   set fontfactor 2
+   set canvFontsize [expr round($::fontsize / $fontfactor)]
    font conf movingTextReposFont -size $canvFontsize
    
   bind $repospic::w <Return> $confirmBtnAction
